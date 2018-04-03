3c8b344dfdea756b8ef3c5dbae5dc460d423faae
jphillip44 <jphillip44@PC.localdomain>
2018-04-03 19:43:10 -0400

    modified script for windows/linux

--- __specs/my_tracker.sh
+++ __specs/my_tracker.sh
@@ -4,7 +4,12 @@ student=../tracker-project/tests/acceptance/student
 instructor=../tracker-project/tests/acceptance/instructor
 s_results=./my_tests/student
 i_results=./my_tests/instructor
-tracker=../tracker-project/EIFGENs/tracker/W_code/tracker
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        tracker=../tracker-project/EIFGENs/tracker/W_code/tracker.exe
+	elif [[ "$OSTYPE" == "linux" ]]; then
+        tracker=../tracker-project/EIFGENs/tracker/W_code/tracker
+	fi
+
 
 mkdir -p $s_results
 mkdir -p $i_results
@@ -13,6 +18,7 @@ for i in $instructor/*
 do
   $tracker -b $i > $i_results/actual.$(basename $i)
   ./oracle.exe -b $i > $i_results/expected.$(basename $i)
+  sed -i.bak 's/\r//g' $i_results/actual.$(basename $i)
   diff -qs $i_results/expected.$(basename $i) $i_results/actual.$(basename $i)
 done
 
@@ -20,6 +26,8 @@ for i in $student/*
 do
   $tracker -b $i > $s_results/actual.$(basename $i)
   ./oracle.exe -b $i > $s_results/expected.$(basename $i)
+  sed -i.bak 's/\r//g' $s_results/actual.$(basename $i)
   diff -qs $s_results/expected.$(basename $i) $s_results/actual.$(basename $i)
 done
 
+rm -f $s_results/* $i_results/*
